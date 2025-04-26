f795513f5a4c3ef279726e2f239687cdfe87ceab
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-26 19:22:13 -0400

    feat: created components home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CategoryTableComponent } from './components/category-table/category-table.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
     path: 'categories',
     component: CategoryTableComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
 ];
