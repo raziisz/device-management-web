e02be3a9faf096ce0c169bf70683bee0519c00aa
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-26 19:04:18 -0400

    feat: configuration app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'device-management-web';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CategoryTableComponent } from './components/category-table/category-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'categories',
+    component: CategoryTableComponent,
+  },
+];
