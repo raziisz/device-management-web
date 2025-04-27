04f2b91a4dd644407e49bde628a44b3cb25631a8
Felipe Libertino <luiz.libertino@conecthus.org.br>
2025-04-26 20:27:48 -0400

    feat: initial device page

---
+++ src/app/pages/device/device.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device',
+  imports: [],
+  templateUrl: './device.component.html',
+  styleUrl: './device.component.css'
+})
+export class DeviceComponent {
+
+}
